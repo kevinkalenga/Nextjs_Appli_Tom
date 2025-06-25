4904b0ace726f53d0627711c64ae192ae1da40c1
kevin kalenga <kevinkalenga10@gmail.com>
2025-06-25 23:58:47 +0200

    Install shadcn ui and add login with google

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,10 @@
+import ContinueWithGoogleButton from "@/components/continue-with-google-button";
+
 // the children in the layout is referecing all the page data for every route
 export default function Login() {
     return (
         <div>
-           Login page
+           <ContinueWithGoogleButton />
         </div>
     )
 }
