3dcbfab06c1d02545d5d07f730b556b5d8a86ab4
kevin kalenga <kevinkalenga10@gmail.com>
2025-06-25 23:38:27 +0200

    Add the navbar with auth links

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,8 @@
+// the children in the layout is referecing all the page data for every route
+export default function Login() {
+    return (
+        <div>
+           Login page
+        </div>
+    )
+}
