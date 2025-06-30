a98d1213dda37e8339de4a38b58524444712a32b
kevin kalenga <kevinkalenga10@gmail.com>
2025-06-30 18:51:33 +0200

    Create PropertyForm component and start building form ui

--- app/admin-dashboard/new/new-property-form.tsx
+++ app/admin-dashboard/new/new-property-form.tsx
@@ -1,11 +1,20 @@
 "use client" 
 
+import PropertyForm from "@/components/property-form"
 import { propertyDataSchema } from "@/validation/propertySchema"
+import {z} from "zod"
 
 
 
 export default function NewPropertyForm() {
+    
+   const handleSubmit = async (data: z.infer<typeof propertyDataSchema>) => {
+
+   }
+    
     return (
-        <div>New Property</div>
+        <div>
+            <PropertyForm handleSubmit={handleSubmit} />
+        </div>
     )
 }
